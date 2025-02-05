e0dfc785ea416678ff2ce6de252bda7cfe468414
github-charles-murphy <d_professor@msn.com>
2025-02-05 00:48:58 +0000

    rename

--- src/main/bus.tf
+++ src/main/bus.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name        = "${var.username}-event-rule"
-  description = "Capture events from the event bus and send them to SQS"
+  description = "Capture SNS events from the event bus"
 
   event_bus_name = local.bus.arn
 
@@ -12,4 +12,13 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
 resource "aws_cloudwatch_event_target" "sqs_target" {
   rule      = aws_cloudwatch_event_rule.event_rule.name
   arn       = module.durable_queue.sqs.arn
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "/${var.username}/${local.bus.name}"
+}
+
+resource "aws_cloudwatch_event_target" "log_target" {
+  rule      = aws_cloudwatch_event_rule.event_rule.name
+  arn       = aws_cloudwatch_log_group.log_group.arn
 }
