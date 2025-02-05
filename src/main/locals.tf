80117a6104316e31928ecae27772e6ef50d148e6
github-charles-murphy <d_professor@msn.com>
2025-02-05 01:24:33 +0000

    fix syntax errors

--- src/main/locals.tf
+++ src/main/locals.tf
@@ -1,13 +1,13 @@
 locals {
   bus = {
-    name = var.bus,
-    arn = "arn:aws:events:${var.region}:${var.accountid}:event-bus/${var.username}-${var.bus}",
+    name    = var.bus
+    arn     = "arn:aws:events:${var.region}:${var.accountid}:event-bus/${var.username}-${var.bus}"
     service = "events.amazonaws.com"
   }
   sns_list = [
     for sns in var.sns_list : {
-      name = sns,
-      arn = "arn:aws:sns:${var.region}:${var.accountid}:${var.username}-${sns}",
+      name    = sns
+      arn     = "arn:aws:sns:${var.region}:${var.accountid}:${var.username}-${sns}"
       service = "pipes.amazonaws.com"
     }
   ]
